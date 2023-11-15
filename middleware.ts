8e93098f12a6130fc0b2d08c2eca53be244ccff9
김우빈 <kwb020312@naver.com>
2023-11-15 23:23:43 +0900

    Feat: Upload Profile Image

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,17 @@ import { authMiddleware } from "@clerk/nextjs";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/api/account",
+    "/api/lead-magnet",
+    "/api/webhooks/stripe",
+    "/api/lead-magnet/publish",
+    "/api/lead-magnet/unpublish",
+    "/api/uploadthing",
+  ],
 });
 
 export const config = {
